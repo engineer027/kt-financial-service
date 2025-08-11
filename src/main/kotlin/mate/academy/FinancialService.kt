021bae8d8f3fc8a88e39fd87fd08f8930cf70da6
Mykhailo <mykhailo.d@air-dr.com>
2025-08-11 17:32:30 +0300

    impl FinancialService

--- src/main/kotlin/mate/academy/FinancialService.kt
+++ src/main/kotlin/mate/academy/FinancialService.kt
@@ -1,5 +1,41 @@
 package mate.academy
 
+@JvmInline
+value class AccountNumber(val number: String) {
+    init {
+        require(number.matches(Regex("^\\d{10}$"))) {
+            "Invalid account number"
+        }
+    }
+}
+
+@JvmInline
+value class CurrencyAmount(val amount: Double) {
+    init {
+        require(amount >= 0.0) {
+            "Amount must be positive"
+        }
+    }
+}
+
+@JvmInline
+value class CurrencyCode(val code: String) {
+    init {
+        require(code.length == 3 && code.matches(Regex("^[A-Z]{3}$"))) {
+            "Invalid code"
+        }
+    }
+}
+
+@JvmInline
+value class TransactionId(val id: String) {
+    init {
+        require(id.isNotEmpty()) {
+            "Invalid transaction id"
+        }
+    }
+}
+
 class FinancialService {
     fun transferFunds(
         source: AccountNumber,
@@ -7,8 +43,12 @@ class FinancialService {
         amount: CurrencyAmount,
         currencyCode: CurrencyCode,
         transactionId: TransactionId
-    ) : String {
-        // TODO: implement
+    ): String {
+        return """
+            Transferred ${amount.amount} ${currencyCode.code}
+            from ${source.number} to ${destination.number}.
+            Transaction ID: ${transactionId.id}
+            """.trimIndent().replace("\n", " ")
     }
 
     fun convertCurrency(
@@ -16,7 +56,7 @@ class FinancialService {
         fromCurrency: CurrencyCode,
         toCurrency: CurrencyCode
     ): CurrencyAmount {
-        // TODO: implement
+        return CurrencyAmount(amount.amount * getExchangeRate(fromCurrency, toCurrency))
     }
 
     private fun getExchangeRate(fromCurrency: CurrencyCode, toCurrency: CurrencyCode): Double {
